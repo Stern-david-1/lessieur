3b416b117f5a07408a8b07c065dea72cd6ba6a25
Stern-david-1 <Davidisidor17@gmail.com>
2026-02-02 09:22:36 -0500

    js

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const btPlus = document.querySelector('.js-plus');
+const conteneur = document.querySelector('.conteneur-transports');
+
+btPlus.addEventListener('click', fouvreferme);
+
+function fouvreferme(){
+    conteneur.classList.toggle('conteneur-transports-invisible');
+}
